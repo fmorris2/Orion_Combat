bd950c69f5b2e02ddf09fdd9c6f2866774dc3514
Dad <Dad>
2017-01-19 22:53:55 -0500

    T

--- src/org/data/enums/equipment/EquipmentIMGenerator.java
+++ src/org/data/enums/equipment/EquipmentIMGenerator.java
@@ -9,6 +9,7 @@ import org.data.enums.equipment.melee.MeleeWeapon;
 import org.osbot.rs07.api.ui.Skill;
 
 import viking.framework.goal.impl.SkillGoal;
+import viking.framework.goal.impl.TimeGoal;
 import viking.framework.item_management.IMEntry;
 import viking.framework.mission.Mission;
 
@@ -34,14 +35,16 @@ public class EquipmentIMGenerator
 			if(armor == MeleeArmor.WOODEN_SHIELD)
 				continue;
 			
-			entries.add(new IMEntry(m, armor.ITEM_ID, 1, armor.PRICE, armor.name().replaceAll("_", " ").toLowerCase(), new SkillGoal(m.skills, Skill.DEFENCE, armor.DEF_LVL)));
+			entries.add(new IMEntry(m, armor.ITEM_ID, 1, armor.PRICE, armor.name().replaceAll("_", " ").toLowerCase(), 
+					new SkillGoal(m.skills, Skill.DEFENCE, armor.DEF_LVL), new TimeGoal(60000 * 7)));
 		}
 		for(MeleeWeapon wep : MeleeWeapon.values())
 		{
 			if(wep == MeleeWeapon.BRONZE_SWORD)
 				continue;
 			
-			entries.add(new IMEntry(m, wep.ITEM_ID, 1, wep.PRICE, wep.name().replaceAll("_", " ").toLowerCase(), new SkillGoal(m.skills, Skill.ATTACK, wep.ATT_REQ)));
+			entries.add(new IMEntry(m, wep.ITEM_ID, 1, wep.PRICE, wep.name().replaceAll("_", " ").toLowerCase(), 
+					new SkillGoal(m.skills, Skill.ATTACK, wep.ATT_REQ), new TimeGoal(60000 * 7)));
 		}	
 		return entries.toArray(new IMEntry[entries.size()]);
 	}
