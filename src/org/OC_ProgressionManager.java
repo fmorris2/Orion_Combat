f66b1ad2c13c6ee9551e7d1d1b6bf39f9623b98f
Dad <Dad>
2017-01-20 23:36:42 -0500

    T

--- src/org/OC_ProgressionManager.java
+++ src/org/OC_ProgressionManager.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.data.enums.LOCATION;
 import org.data.enums.MONSTER;
 import org.data.enums.equipment.Food;
-import org.data.monster.Monster;
 import org.osbot.rs07.script.MethodProvider;
 
 public class OC_ProgressionManager
@@ -24,7 +23,7 @@ public class OC_ProgressionManager
 		final MONSTER APPROP = MONSTER.getAppropriate(mission.combat);
 		
 		final MONSTER OLD = mission.getMonsterEnum() == null ? null : mission.getMonsterEnum();
-		final MONSTER TARGET = mission.TARGET == null ? null : mission.TARGET;
+		final MONSTER TARGET = mission.TARGET == null ? null : mission.TARGET;	
 		
 		if(OLD != null && (OLD == TARGET || OLD == APPROP))
 			return false;
@@ -32,14 +31,27 @@ public class OC_ProgressionManager
 		if(OLD != null && OLD.MONSTER.REQUIRED_COMBAT_LVL == APPROP.MONSTER.REQUIRED_COMBAT_LVL && APPROP != TARGET)
 			return false;
 		
-		mission.setMonster(APPROP);
-		
+		if(TARGET != null && mission.combat.getCombatLevel() >= TARGET.MONSTER.REQUIRED_COMBAT_LVL)
+		{
+			mission.setMonster(TARGET);
+			setLocation(TARGET);
+		}
+		else
+		{
+			mission.setMonster(APPROP);
+			setLocation(APPROP);
+		}
 		//TODO QUERY OCC FOR APPROPRIATE LOCATION
-		List<LOCATION> supported = MONSTER.getSupportedLocs(APPROP);
-		mission.setLocation(supported.get(MethodProvider.random(0, supported.size() - 1)));
+		
 		
 		mission.getScript().log(this, false, "Progessing to " + APPROP + " at location " + mission.getLocEnum());
 		
 		return true;
+	}
+	
+	private void setLocation(MONSTER m)
+	{
+		List<LOCATION> supported = MONSTER.getSupportedLocs(m);
+		mission.setLocation(supported.get(MethodProvider.random(0, supported.size() - 1)));
 	}
 }
