330bbd361506af4ed7054179997f64b2cd3a77e6
Final Calibur <fred11212@hotmail.com>
2017-01-25 00:33:26 -0500

    Bug fixes

--- src/org/worker/impl/bank/OC_WithdrawUpgrades.java
+++ src/org/worker/impl/bank/OC_WithdrawUpgrades.java
@@ -26,10 +26,17 @@ public class OC_WithdrawUpgrades extends OCWorker
 			bankUtils.open();
 		else
 		{
-			List<Integer> upgrades = mission.EQUIPMENT_MANAGER.getUpgrades();
-			for(int i : upgrades)
-				if(!inventory.contains(i))
-					bank.withdraw(i, 1);
+			try
+			{
+				List<Integer> upgrades = mission.EQUIPMENT_MANAGER.getUpgrades();
+				for(int i : upgrades)
+					if(!inventory.contains(i))
+						bank.withdraw(i, 1);
+			}
+			catch(ArrayIndexOutOfBoundsException e)
+			{
+				script.log(this, false, "There was an ArrayIndexOutOfBoundsException thrown by bank.withdraw in the OSBot API");
+			}
 		}
 		
 	}
